705844ef4d7cb1b028fa9e11fa275fc1d9e261cc
Sikhun <nasikhunamin582@gmail.com>
2020-09-04 20:14:33 +0700

    renew

--- app/src/main/java/com/monasikamin/shimmerlayout/Api.kt
+++ app/src/main/java/com/monasikamin/shimmerlayout/Api.kt
@@ -5,6 +5,6 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface Api {
-    @GET("discover/movie?language=en-US&api_key=854f1436ecee23a37aee7e3b3eae9d49")
+    @GET("discover/movie?language=en-US&api_key="+BuildConfig.TMDB_API_KEY)
     fun getMovies (@Query("page") page: Int?) : Call<MovieResponse>
 }
